19e79bdd1c6709b131181429a70c4738c5ebff77
Fabrice <fabrice_valade@hotmail.com>
2022-10-21 23:50:47 -0400

    start putting messages in a queue

--- src/macroCom.js
+++ src/macroCom.js
@@ -29,64 +29,38 @@ export async function runArbitraryMacro(MacroName, Args = null) {
 
 export const messager ={
     postUpdate: (message) => {
-        let sentInfo;
-        switch(message.action){
-            case(messaging.remove):
-                sentInfo = makeRemoveMessage(message)
-                break
-            case(messaging.add):
-                sentInfo = makeAddMessage(message)
-                break
-            case(messaging.clear):
-            case(messaging.roll):
-                return
-            case(messaging.rollResult):
-                sentInfo = makeRollResultMessage(message)
-                break
-            case(messaging.new):
-                sentInfo = `New tray was filled for throwing <br> ${formatDiceListHeaderOnly(message.diceList)}`
-                break
-
-            default:
-                sentInfo = "Received unknown message: "+message.action
+        messageQueue.push(message)
+        if(!pendingTimeout){
+            pendingTimeout = setTimeout(() => {
+                clearQueue()
+                pendingTimeout=null
+            }, 2500);
         }
-
-        sentInfo= `<table style="border:1px solid black; margin: 0px; padding: 2px"><tr><td>${sentInfo}</td></tr></table>` 
-
-
-        if (process.env.NODE_ENV != 'production'){
-            console.log(sentInfo)
-            return null
-        
-        }
-        runArbitraryMacroJson("broadcast",sentInfo)
     }
+        
 }
 
 const makeRemoveMessage = (message) => {
     const {target, diceList} = message
     const value = target.rolling ? "was rolling" : `had value ${target.value}`
     const type = target.display ? target.display : `1d${target.maximum}`
-    const tray = diceList?.length ? formatDiceListInHtml(diceList) : "Tray empty"
-    return `Removed a ${type} which ${value} <br> ${tray}`
+    return `Removed a ${type} which ${value}`
 }
 const makeAddMessage = (message) => {
     const {target, diceList} = message
     const type = target.display ? target.display : `1d${target.maximum}`
-    const tray = formatDiceListInHtml(diceList,target)
-    return `Added a ${type} <br> ${tray}`
+    return `Added a ${type}`
 }
 
 const makeRollResultMessage = (message) => {
     const {target, diceList} = message
     const type = target.display ? target.display : `1d${target.maximum}`
-    const tray = formatDiceListInHtml(diceList,target)
-    return `${type} finished rolling. <br> ${tray}`
+    return `${type} finished rolling.`
 }
 
-const formatDiceListInHtml = (diceList, markedDice) =>{
+const formatDiceListInHtml = (diceList, markedDice=null) =>{
     const wrappedDices = diceList.map(dice => {
-        const color = dice.id === markedDice.id ? "bgcolor=aqua" : ""
+        const color = dice.id === markedDice?.id ? "bgcolor=aqua" : ""
         return `<table width=40 style="border:1px solid black; margin: 0px; padding: 0px">
                 <tr><td bgcolor=yellow style="margin: 0px; padding: 0px">${dice.display}</td></tr>
                 <tr><td ${color} align=center valign=middle style="margin: 0px; padding: 0px">${dice.rolling ? "?" : dice.value}</td></tr>
@@ -130,4 +104,55 @@ const messaging={
     rollAll:"rollAll",
     clear:"clear",
     new:"new"
+}
+
+let pendingTimeout = null
+let messageQueue = []
+  
+  
+function clearQueue(){
+    sendAQueue(messageQueue)
+    messageQueue=[]
+    return
+}
+
+async function sendAQueue(queue){
+    let sentInfo=""
+    const targeted=[]
+    queue.forEach(message => {
+        switch(message.action){
+            case(messaging.remove):
+                sentInfo+= makeRemoveMessage(message)
+                break
+            case(messaging.add):
+                sentInfo+= makeAddMessage(message)
+                break
+            case(messaging.clear):
+            case(messaging.roll):
+                return
+            case(messaging.rollResult):
+                sentInfo+= makeRollResultMessage(message)
+                break
+            case(messaging.new):
+                sentInfo+= `New tray was filled for throwing <br> ${formatDiceListHeaderOnly(message.diceList)}`
+                break
+            default:
+                sentInfo = "Received unknown message: "+message.action  
+        }
+        message.target != null ? targeted.push(message.target) : false
+    });
+    const lastmessage = queue.pop()
+    const tray = lastmessage?.diceList?.length ? formatDiceListInHtml(lastmessage.diceList) : "Tray empty"
+
+
+    sentInfo= `<table style="border:1px solid black; margin: 0px; padding: 2px">
+                <tr><td>${sentInfo}</td></tr>
+                <tr><td>${tray}</td></tr>
+                </table>` 
+    if (process.env.NODE_ENV != 'production'){
+        console.log(sentInfo)
+        return null
+    
+    }
+    runArbitraryMacroJson("broadcast",sentInfo)
 }
